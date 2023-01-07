c8f42821dd1c9a5578f32e290f1aa1267bd8b78a
Ronaldo Silva <ronaldo.intermec@gmail.com>
2023-01-07 00:06:07 -0300

    my first commit

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/'],
+};
+
+//rodar o seguinte comando no terminal: yarn react-native link ou npx react-native-asset
